2ea1bf3ef38bedeb49994be7ae5eeb42b7cfb628
tester <test@realtalk.local>
2026-06-08 08:41:05 +0800

    fix: set iOS apiBaseURL to host public address 125.227.239.165:18764

--- realtalk/realtalk/AppConfig.swift
+++ realtalk/realtalk/AppConfig.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum AppConfig {
-    static let apiBaseURL = URL(string: "http://127.0.0.1:8000")!
+    static let apiBaseURL = URL(string: "http://125.227.239.165:18764")!
     static let subscriptionProductID = "realtalk.pro.monthly"
     static let localRetentionDays = 3
 }
